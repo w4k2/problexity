d84dfc0f53ae0a87787c7de25f6e015d2ab33bfc
Joana <asiakom24@vp.pl>
2022-06-17 14:59:29 +0200

    Update documentation

--- problexity/linearity.py
+++ problexity/linearity.py
@@ -8,17 +8,19 @@ def L1(X, y):
     """
     Calculates the Sum of the error distance by linear programming (L1) metric.
 
+    Uses Linear SVM classifier. Measures distance of incorrectly classified samples from the SVM hyperplane.
+
     .. math::
 
         L1=\\frac{SumErrorDist}{1+SumErrorDist}
 
     :type X: array-like, shape (n_samples, n_features)
-    :param X: Dataset.
+    :param X: Dataset
     :type y: array-like, shape (n_samples)
-    :param y: Labels.
+    :param y: Labels
 
     :rtype: float
-    :returns: L1 score.
+    :returns: L1 score
     """    
     X = np.copy(X)
     y = np.copy(y)
@@ -38,17 +40,19 @@ def L2(X, y):
     """
     Calculates the Error rate of linear classifier (L2) metric.
 
+    Returns error rate of Linear SVM classifer used within the dataset.
+
     .. math::
 
         L2=\\frac{\sum^{n}_{i=1}I(h(x_i)\\neq y_i)}{n}
 
     :type X: array-like, shape (n_samples, n_features)
-    :param X: Dataset.
+    :param X: Dataset
     :type y: array-like, shape (n_samples)
-    :param y: Labels.
+    :param y: Labels
 
     :rtype: float
-    :returns: L2 score.
+    :returns: L2 score
     """
 
     X = np.copy(X)
@@ -64,17 +68,19 @@ def L3(X, y):
     """
     Calculates the Non linearity of linear classifier (L3) metric.
 
+    Linearly interpolating instances of each class generate the additional instances of the problem, which are used to calculate this measure. The class of original instances determines the label of an augmented point. The Linear SVM classifier is used to classify the synthesized points of the dataset. The number of synthetic points is equal to the original dataset size.
+    
     .. math::
 
         L3=\\frac{1}{l}\sum^{l}_{i=1}I(h_T(x'_i) \\neq y'_i)
 
     :type X: array-like, shape (n_samples, n_features)
-    :param X: Dataset.
+    :param X: Dataset
     :type y: array-like, shape (n_samples)
-    :param y: Labels.
+    :param y: Labels
 
     :rtype: float
-    :returns: L3 score.
+    :returns: L3 score
     """
     
     X = np.copy(X)
