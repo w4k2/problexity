e22af2bb2d557a8dcfc9ebebcc8b0cc853ddbeba
Joana <asiakom24@vp.pl>
2022-10-06 11:33:36 +0200

    Import classification measures as default in init

--- problexity/__init__.py
+++ problexity/__init__.py
@@ -1,9 +1,37 @@
 from . import classification
 from . import regression
+from .classification.feature_based import f1, f1v, f2, f3, f4
+from .classification.linearity import l1, l2, l3
+from .classification.neighborhood import n1, n2, n3, n4, t1, lsc
+from .classification.network import density, clsCoef, hubs
+from .classification.dimensionality import  t2, t3, t4
+from .classification.class_imbalance import c1, c2
 from .ComplexityCalculator import ComplexityCalculator
 from ._version import __version__
 
 __all__ = [
     "ComplexityCalculator",
     "__version__",
+    "f1", 
+    "f1v", 
+    "f2",
+    "f3",
+    "f4",
+    "l1",
+    "l2",
+    "l3",
+    "n1",
+    "n2",
+    "n3",
+    "n4",
+    "t1",
+    "lsc",
+    "density",
+    "clsCoef",
+    "hubs",
+    "t2",
+    "t3",
+    "t4",
+    "c1",
+    "c2"
 ]
